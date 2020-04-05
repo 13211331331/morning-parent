b87d68f067b6dd693abac1025ff2c1f956e3050b
your name <email@example.com>
2020-04-05 20:51:35 +0800

    dd

--- morning-product/morning-product-service/src/main/java/org/pussinboots/morning/product/mapper/ProductMapper.java
+++ morning-product/morning-product-service/src/main/java/org/pussinboots/morning/product/mapper/ProductMapper.java
@@ -2,7 +2,6 @@ package org.pussinboots.morning.product.mapper;
 
 import com.baomidou.mybatisplus.plugins.Page;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.session.RowBounds;
 import org.pussinboots.morning.common.support.page.PageInfo;
 import org.pussinboots.morning.product.entity.Product;
 import org.pussinboots.morning.product.pojo.vo.ProductVO;
@@ -12,23 +11,29 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
 import java.util.List;
 
 /**
- * 
-* 项目名称：morning-product-service   
-* 类名称：ProductMapper   
-* 类描述：Product / 商品表 数据访问层接口      
-* 创建人：陈星星   
-* 创建时间：2017年4月11日 下午3:15:04   
-*
+ *
+ * 项目名称：morning-product-service
+ * 类名称：ProductMapper
+ * 类描述：Product / 商品表 数据访问层接口
+ * 创建人：yeungchihang
+ * 创建时间：2017年4月11日 下午3:15:04
+ *
  */
 public interface ProductMapper extends BaseMapper<Product> {
-	
-	/**
-	 * 根据商品编号和状态查找商品
-	 * @param productNumber 商品编号
-	 * @param status 状态
-	 * @return
-	 */
-	ProductVO getByNumber(@Param("productNumber") Long productNumber, @Param("status") Integer status);
 
-    List<Product> lisByPage(@Param("pageInfo") PageInfo pageInfo, @Param("search") String search, RowBounds rowBounds);
+    /**
+     * 根据商品编号和状态查找商品
+     * @param productNumber 商品编号
+     * @param status 状态
+     * @return
+     */
+    ProductVO getByNumber(@Param("productNumber") Long productNumber, @Param("status") Integer status);
+
+    List<Product> listByPage(PageInfo pageInfo, String search, Page<Product> page);
+
+    Long getIdByNumber(@Param("productNumber") Long productNumber);
+
+    Product getByProductId(Long productId);
+
+    Integer insertProduct(Product product);
 }
